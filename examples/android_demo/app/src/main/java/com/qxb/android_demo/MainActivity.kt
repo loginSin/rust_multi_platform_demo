40986c88631d805a3df1c09d3c1a6afe34b57fc8
qixinbing <qixinbing@rongcloud.cn>
2025-06-18 10:09:03 +0800

    feat: init android_demo.

---
+++ examples/android_demo/app/src/main/java/com/qxb/android_demo/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.qxb.android_demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.qxb.android_demo.ui.theme.Android_demoTheme
+import com.qxb.mynativelib.NativeLib
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        var str = NativeLib().stringFromJNI()
+        setContent {
+            Android_demoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_demoTheme {
+        Greeting("Android")
+    }
+}
---
+++ examples/android_demo/mynativelib/src/main/java/com/qxb/mynativelib/NativeLib.java
@@ -0,0 +1,15 @@
+package com.qxb.mynativelib;
+
+public class NativeLib {
+
+    // Used to load the 'mynativelib' library on application startup.
+    static {
+        System.loadLibrary("mynativelib");
+    }
+
+    /**
+     * A native method that is implemented by the 'mynativelib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
